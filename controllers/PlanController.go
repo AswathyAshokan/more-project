8fa439a98e649916770245a842ad69b5bb8ab18a
Farsana <farsana.pb@cynere.com>
2017-02-24 12:51:21 +0530

    24/2/2017 12:51  Changing url kor Identifying logined admin

--- controllers/PlanController.go
+++ controllers/PlanController.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"app/passporte/viewmodels"
 
-	"reflect"
 )
 
 type PlanController struct {
@@ -16,38 +15,22 @@ type PlanController struct {
 
 func (c *PlanController) PlanDetails() {
 	r := c.Ctx.Request
-	//w := c.Ctx.ResponseWriter
-	//storedSession := ReadSession(w, r)
 	w := c.Ctx.ResponseWriter
 	storedSession := ReadSession(w, r)
 	log.Println("The userDetails stored in session:",storedSession)
-	reflect.TypeOf(storedSession)
-
 	log.Println("session:", storedSession)
 	viewModel := viewmodels.Plan{}
-	//sessionValues := SessionValues{}
-
 	if cookie, err := r.Cookie("session"); err == nil {
 		log.Println("cookie",cookie)
 		viewModel.SessionFlag = true
 	} else {
 		viewModel.SessionFlag = false
 	}
-
-	/*viewModel.Email = storedSession.AdminEmail
-	viewModel.FirstName = storedSession.AdminFirstName
-	viewModel.SecondName = storedSession.AdminLastName*/
-	log.Println("ggg",viewModel)
 	c.Data["vm"] = viewModel
 	c.TplName = "template/plan.html"
 }
 
 
 func (c *PlanController) PlanCheck() {
-	/*log.Println("fff")
-	r := c.Ctx.Request
-	w := c.Ctx.ResponseWriter
-	storedSession := ReadSession(w, r)
-
-	log.Println(storedSession.Info.CompanyName)*/
+	log.Println("ghdgfhdgfdhg")
 }
