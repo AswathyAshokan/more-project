773c9b93dd4dba875bafea6837e8261cd09d8e75
AswathyAsokan <aswathy.a@cynere.com>
2017-04-20 12:07:50 +0530

    13/04/2017  login page

---
+++ main/static/js/custom/edit-profile.js
@@ -0,0 +1,202 @@
+
+var companyTeamName = vm.CompanyTeamName;
+var thumbnail = "";
+var file ="";
+var img ="";
+var unixDateTime ="";
+var thumbUrl = ""; 
+var profileUrl = "";
+var tempProfilePicture ="";
+var tempThumbPicture ="";
+ 
+//function for displaying image
+function displayImage() {
+    file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+    reader.onloadend = function () {
+        document.getElementById("imageUpload").src = reader.result;
+    }
+    console.log("newww",document.getElementById("imageUpload").src);
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        document.getElementById("imageUpload").src = "";
+    }   
+}
+//uploading image
+var config = {
+            apiKey: "AIzaSyDME5QGEf2AZd0eJGf5NAzOqKui7RtH4qc",
+            authDomain: "passporte-b9070.firebaseapp.com",
+            databaseURL: "https://passporte-b9070.firebaseio.com",
+            projectId: "passporte-b9070",
+            storageBucket: "passporte-b9070.appspot.com",
+            messagingSenderId: "196354561117"
+        };
+firebase.initializeApp(config);
+
+
+function resizeImg() {
+    console.log("inside");
+    img  = document.querySelector('input[type=file]').files[0];
+    img.height = 200;
+    img.width = 100;
+}
+$().ready(function() {
+    
+    document.getElementById("name").value = vm.FirstName;
+    document.getElementById("emailId").value = vm.Email;
+    document.getElementById("phoneNumber").value = vm.PhoneNo;
+    
+    //to check the plan and load modal according to plan
+    if(vm.CompanyPlan == "family")
+        {
+            $('#planChange').attr('data-target','#family');
+        } else if (vm.CompanyPlan == "campus") {
+            $('#planChange').attr('data-target','#campus');
+        }else if (vm.CompanyPlan == "business") {
+            $('#planChange').attr('data-target','#business');
+        }else if (vm.CompanyPlan == "businessPlus") {
+           $('#planChange').attr('data-target','#business-plus');
+        }
+    
+    
+    //function for editing form
+   $('#edit-txt').on('click', function() {
+        var btntxt = $("#edit-txt").text();
+        if (btntxt == 'Edit') {
+            $(".edit-account input").prop( "disabled", false );
+            $(".edit-account input").toggleClass("dis-txt");	
+            $('#edit-txt').text("Save");
+            $('#edit-txt').attr('type', 'submit');
+            return false;
+        }
+        $("#adminAccountDetail").validate({
+            rules: {
+                name:"required",
+                emailId:{
+                    required:true,
+                    email:true
+                },
+                phoneNumber: "required"
+            },
+            messages: {
+                name:"Please enter your Name ",
+                emailId: "Please enter Phone Number",
+                phoneNumber:"Please enter your Email id"
+            },
+            
+            submitHandler: function(){//to pass all data of a form serial
+                
+                //image upload
+                //var imageNumber =0;
+                 var now = new Date();
+                var datetime = now.getFullYear()+'/'+(now.getMonth()+1)+'/'+now.getDate(); 
+                datetime += ' '+now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
+                unixDateTime = Date.parse(datetime)/1000;
+//                tempProfilePicture  =file.name(' ').join('_');
+//                console.log("temp",tempProfilePicture);
+                var storageRef = firebase.storage().ref('profilePicturesOfAbmin/original/'+unixDateTime+file.name);
+                storageRef.put(file);
+                function error(err) {
+                    console.log("error",err);
+                }
+                
+                //retrieve image from firebase storage
+                //tempThumbPicture  =img.name(' ').join('_');
+                var storageRef = firebase.storage().ref('profilePicturesOfAbmin/thumbnail/'+unixDateTime+img.name);
+                storageRef.put(img);
+                function error(err) {
+                    console.log("error",err);
+                }
+                
+                var displayThumbRef = firebase.storage().ref('profilePicturesOfAbmin/thumbnail/'+unixDateTime+img.name);
+                setTimeout(function() { displayThumbRef.getDownloadURL().then(function(url) {
+                    // Get the download URL for 'images/stars.jpg'
+                    // This can be inserted into an <img> tag
+                    thumbUrl=url;
+                   
+                }).catch(function(error) {
+                    console.error(error);
+                }); }, 3000);
+                 var displayProfileRef = firebase.storage().ref('profilePicturesOfAbmin/original/'+unixDateTime+file.name);
+                setTimeout(function() { displayProfileRef.getDownloadURL().then(function(url) {
+                    // Get the download URL for 'images/stars.jpg'
+                    // This can be inserted into an <img> tag
+                    profileUrl=url;
+                    
+                }).catch(function(error) {
+                    console.error(error);
+                }); }, 2000);
+                setTimeout(function(){var formData = $("#adminAccountDetail").serialize()+ "&profilePicture=" + profileUrl+"&profilePicturePath=" + file+"&thumbPicture=" + thumbUrl;
+                                      console.log("thumb",thumbUrl);
+                                      console.log("profile",profileUrl);
+                                      $.ajax({
+                                          url:'/'+ companyTeamName + '/editProfile',
+                                          type:'post',
+                                          datatype: 'json',
+                                          data: formData,
+                    //call back or get response here
+                                          success : function(response){
+                                              if(response == "true"){
+                                                  $('#edit-txt').text("Edit");
+                                                  $(".edit-account input").prop( "disabled", true );
+                                                  $('#edit-txt').attr('type', 'button');
+                                              } else {
+                                                  $('#edit-txt').text("Edit");
+                                              }
+                                          },
+                                          error: function (request,status, error) {
+                                          }
+                                      });
+                                      return false;},5000);
+            }
+        });
+   });
+
+    //function to change password
+    
+     $('#updateAdminPassword').on('click', function() {
+        $("#adminPasswordChangeModal").validate({
+            rules: {
+                newPassword:"required",
+                confirmpassword:{
+                    equalTo : "#newPassword"
+                } ,
+                oldPassword: {
+                required: true,
+                remote:{
+                    url: '/'+ companyTeamName +"/isOldAdminPasswordCorrect/" + oldPassword,
+                    type: "post"
+                }
+            },
+            },
+            messages: {
+                 oldPassword:{
+                     required: "Please enter Old Password ",
+                     remote: "The password entered is not correct !!!"
+                 },
+                newPassword: "Please enter New Password",
+                confirmpassword:"Retype password is incorrect"
+            },
+            submitHandler: function(){//to pass all data of a form serial
+                var formData = $("#adminPasswordChangeModal").serialize();
+                $.ajax({
+                    url:'/'+ companyTeamName +'/changePassword',
+                    type:'post',
+                    datatype: 'json',
+                    data: formData,
+                    success : function(response){
+                        if(response == "true"){
+                            window.location = '/'+ companyTeamName + '/editProfile';
+                        } else {
+                            alert("password incorrect");
+                        }
+                    },
+                    error: function (request,status, error) {
+                    }
+                });
+                return false;
+            }
+        });
+    });
+});
---
+++ main/static/js/custom/leave-details.js
@@ -0,0 +1,147 @@
+console.log(vm);
+/* Author :Aswathy Ashok */
+//Below line is for adding active class to layout side menu..
+document.getElementById("leave").className += " active";
+ var companyTeamName = vm.CompanyTeamName;
+$(function(){ 
+    
+    var mainArray = [];   
+    var table = "";
+    function createDataArray(values, keys){
+        var subArray = [];
+        for(i = 0; i < values.length; i++) {
+            for(var propertyName in values[i]) {
+                subArray.push(values[i][propertyName]);
+            }
+            subArray.push(keys[i])
+            mainArray.push(subArray);
+            subArray = [];
+            
+        }
+    }
+    function dataTableManipulate(){
+        table =  $("#leave_details").DataTable({
+            data: mainArray,
+            "searching": false,
+            "paging": true, 
+            "info": false,
+            "lengthChange":false,
+            "columnDefs": [{
+                "targets": -1,
+                "width": "5%",
+                "data": null,
+                "defaultContent": '<button class="btn btn-primary btn-xs " id ="accept">Accept</button>'+"  "+'<button class="btn btn-danger btn-xs " id="reject">Reject</button>'
+            }]
+        });
+        
+    }
+    if(vm.Values != null) {
+        createDataArray(vm.Values, vm.Keys);
+    }
+    dataTableManipulate();
+    
+    //function when click on accept button
+    $('#leave_details').on( 'click', '#accept', function () {
+        var data = table.row( $(this).parents('tr') ).data();
+        var leaveKey = data[5];
+        var userKey =data[6];
+        //alert(data[4]);
+       // window.location = '/'+ companyTeamName +'/leave/' + leaveKey +'/'+userKey+ '/edit';
+        $.ajax({
+                type: "GET",
+                url: '/'+ companyTeamName +'/leave/' + leaveKey +'/'+userKey+ '/accept',
+                data: '',
+                success: function(data){
+                    if(data=="true"){
+                        $('#leave_details').dataTable().fnDestroy();
+                        var index = "";
+                        
+                        for(var i = 0; i < mainArray.length; i++) {
+                           index = mainArray[i].indexOf(leaveKey);
+                           if(index != -1) {
+                               console.log("dddd", i);
+                             break;
+                           }
+                        }
+                        mainArray.splice(i, 1);
+                        dataTableManipulate(mainArray);   
+                    }
+                    else {
+                        console.log("Updation Failed!");
+                    }
+                }
+
+            });
+    });
+    
+    //function when click on reject button
+    $('#leave_details').on( 'click', '#reject', function () {
+        var data = table.row( $(this).parents('tr') ).data();
+        var leaveKey = data[5];
+        var userKey =data[6];
+        //alert(data[4]);
+       // window.location = '/'+ companyTeamName +'/leave/' + leaveKey +'/'+userKey+ '/edit';
+        $.ajax({
+                type: "GET",
+                url: '/'+ companyTeamName +'/leave/' + leaveKey +'/'+userKey+ '/reject',
+                data: '',
+                success: function(data){
+                    if(data=="true"){
+                        $('#leave_details').dataTable().fnDestroy();
+                        var index = "";
+                        
+                        for(var i = 0; i < mainArray.length; i++) {
+                           index = mainArray[i].indexOf(leaveKey);
+                           if(index != -1) {
+                               console.log("dddd", i);
+                             break;
+                           }
+                        }
+                        mainArray.splice(i, 1);
+                        dataTableManipulate(mainArray);   
+                    }
+                    else {
+                        console.log("Updation Failed!");
+                    }
+                }
+
+            });
+    });
+
+
+//    $('#leave_details tbody').on( 'click', '#delete', function () {
+//        $("#myModal").modal();
+//        var data = table.row( $(this).parents('tr') ).data();
+//        var key = data[6];
+//        
+//        $("#confirm").click(function(){
+//            $.ajax({
+//                type: "POST",
+//                url: '/' + companyTeamName + '/contact/' + key + '/delete',
+//                data: '',
+//                success: function(data){
+//                    if(data=="true"){
+//                        $('#leave_details').dataTable().fnDestroy();
+//                        var index = "";
+//                        
+//                        for(var i = 0; i < mainArray.length; i++) {
+//                           index = mainArray[i].indexOf(key);
+//                           if(index != -1) {
+//                             break;
+//                           }
+//                        }
+//                        mainArray.splice(i, 1);
+//                        dataTableManipulate();   
+//                    }
+//                    else {
+//                        console.log("Removing Failed!");
+//                    }
+//                }
+//
+//            });
+//        });
+//    });
+    
+});
+
+
