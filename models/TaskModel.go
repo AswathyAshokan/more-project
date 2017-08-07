7db431ea50d229ac72f96ba1b4d67a21a7db4aac
AswathyAsokan <aswathy.a@cynere.com>
2017-08-07 11:30:31 +0530

    1/06/2013:28:   check box

--- models/TaskModel.go
+++ models/TaskModel.go
@@ -233,7 +233,7 @@ func (m *Tasks) AddTaskToDB(ctx context.Context  ,companyId string ,WorkBreakSli
 		userNotificationDetail.IsViewed ="false"
 		userNotificationDetail.TaskId =taskUniqueID
 		userNotificationDetail.TaskName =m.Info.TaskName
-		err = dB.Child("/Users/"+key.String()+"/Notifications/Tasks/"+taskUniqueID).Set(userNotificationDetail)
+		err = dB.Child("/Users/"+key.String()+"/Settings/Notifications/Tasks/"+taskUniqueID).Set(userNotificationDetail)
 		if err!=nil{
 			log.Println("Insertion error:",err)
 			return false
