429f597c2b606efaa2694df2dcb733f5433dfdeb
AswathyAsokan <aswathy.a@cynere.com>
2017-09-13 16:07:14 +0530

    1/06/2013:28:   work location

--- controllers/DuressController.go
+++ controllers/DuressController.go
@@ -5,6 +5,5 @@ type DuressController struct{
 	BaseController
 }
 func (c *DuressController) Duress(){
-	c.Layout = "layout/layout.html"
-	c.TplName = "template/duress-mode.html"
+	c.TplName = "template/duress.html"
 }
