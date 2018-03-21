c40f7f65a004fd347477d13afac08144a60b8806
AswathyAshokan <aswathy.a@cynere.com>
2018-03-21 14:13:38 +0530

    Update company-fileUpload.js

--- main/static/js/custom/company-fileUpload.js
+++ main/static/js/custom/company-fileUpload.js
@@ -55,15 +55,7 @@ $(function(){
 
 
 //db configuration
-    var config = {
-        apiKey: "AIzaSyDME5QGEf2AZd0eJGf5NAzOqKui7RtH4qc",
-        authDomain: "passporte-b9070.firebaseapp.com",
-        databaseURL: "https://passporte-b9070.firebaseio.com",
-        projectId: "passporte-b9070",
-        storageBucket: "passporte-b9070.appspot.com",
-        messagingSenderId: "196354561117"
-    };
-    firebase.initializeApp(config);
+   
 
 /*Function for creating Data Array for data table*/
 $().ready(function() {
